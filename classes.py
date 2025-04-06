1b85f59b450e192fe2c0b86bcb59d21ba77d7155
Tianze Min <tmin0129@uni.sydney.edu.au>
2025-04-06 18:16:24 +1000

    Implemented prototype RELU and GELU

--- classes.py
+++ classes.py
@@ -1,23 +1,26 @@
 import numpy as np
+import scipy
 import os
 import random
 from matplotlib import pyplot as plt
 
 data_dir = "../Assignment1-Dataset/"
+random.seed(100)
 
 
+""" Custom DataSet and DataLoader classes"""
 class DataSet:
     def __init__(self, data_file : str, label_file : str):
         self.data = np.load(data_file)
         self.labels = np.load(label_file)
     
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.data)
     
-    def __shape__(self):
-        return (self.data.shape[0], self.data.shape[1], self.labels.shape[1])
+    def __shape__(self) -> tuple:
+        return self.data.shape[0], self.data.shape[1], self.labels.shape[1]
         
-    def __getitem__(self, idx):
+    def __getitem__(self, idx) -> tuple:
         return self.data[idx], self.labels[idx]
 
 
@@ -34,7 +37,7 @@ class DataLoader:
         self.current_idx = 0
         return self
     
-    def __next__(self):
+    def __next__(self) -> list:
         if self.current_idx >= len(self.indices):
             raise StopIteration
         
@@ -48,9 +51,41 @@ class DataLoader:
 
         return batch
 
-        
 
+""" Activation Function Classes"""
+class RELU:
+    def __init__(self):
+        self.input = -1
 
+    def __call__(self, x):
+        return self.forward(x)
+
+    def forward(self, x):
+        self.input = x
+        return np.maximum(0, x)
+
+    def backward(self, grad_x):
+        return grad_x if self.input > 0 else 0
+    
+
+class GELU:
+    def __init__(self):
+        self.input = None
+
+    def __call__(self, x):
+        return self.forward(x)
+    
+    def forward(self, x):
+        self.input = x
+        return x * 0.5 * (1 + scipy.special.erf(x / np.sqrt(2)))
+
+    def backward(self, grad_x):
+        x = self.input
+        phi = (1.0 / np.sqrt(2 * np.pi)) * np.exp(-0.5 * np.pow(x, 2))
+        Phi = 0.5 * (1 + scipy.special.erf(x / np.sqrt(2)))
+        return grad_x * (x * phi + Phi)
+
+    
 train_dataset = DataSet(data_dir + "train_data.npy", data_dir + "train_label.npy")
 test_dataset = DataSet(data_dir + "test_data.npy", data_dir + "test_label.npy")
 
@@ -58,4 +93,20 @@ print(train_dataset.__shape__(), test_dataset.__shape__())
 # print(train_dataset.__getitem__(11))
 # print(test_dataset.__getitem__(90))
 
+train_dataloader = DataLoader(train_dataset)
+test_dataloader = DataLoader(test_dataset, 1, False)
+
+# for idx, batch in enumerate(test_dataloader):
+#     print(batch)
+#     break
         
+relu = RELU()
+gelu = GELU()
+
+print(relu(1.13))
+print(relu(-92.85))
+print(relu(-0.35))
+
+print(gelu(1.13))
+print(gelu(-93.85))
+print(gelu(-0.35))
